11d61d4c5da411718ac108b07b4c2f1a0188b38a
natorise <natoriseishot@gmail.com>
2025-07-18 14:40:52 -0400

    add mobile styling

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,31 +1,56 @@
-import { Link } from "wouter"
-import styles from "./NavBar.module.css"
-import { botInvite, serverInvite } from "../modules/links"
+import { Link } from "wouter";
+import styles from "./NavBar.module.css";
+import { botInvite, serverInvite } from "../modules/links";
+
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoMdClose } from "react-icons/io";
+import { useState } from "preact/hooks";
 
 const NavBar = () => {
-  return (
-    <div className={styles.navbar}>
-      
+  const [showNav, setShowNav] = useState(false);
 
-        <Link className={styles.name} href="/">P2 Assistant</Link>
-      
-      
-        <nav>
+  const toggleNav = () => {
+    setShowNav((prev) => !prev);
+  };
+
+  const openClass = showNav ? styles.open : "";
+
+  const hamburgerIcon = showNav ? <IoMdClose size={60} /> :<GiHamburgerMenu size={60} />
+
+  return (
+    <div>
+      <div className={`${styles.navbar} ${openClass}`}>
+        <div className={styles.home}>
+          <Link className={styles.name} href="/">
+            P2 Assistant
+          </Link>
+          <div className={styles.hamburger} onClick={toggleNav}>
+            {hamburgerIcon}
+          </div>
+        </div>
+
+        <nav className={`${styles.nav}`}>
           <ul className={styles.navLinks}>
             {/* <li><a href="/#features">Features</a></li> */}
             {/* <li><Link href="/#commands">Commands</Link></li> */}
-            <li><a href={serverInvite}>Support</a></li>
-            <li><Link href="/tos">TOS</Link></li>
-            <li><Link href="/privacy">Privacy Policy</Link></li>
+            <li>
+              <a href={serverInvite}>Support</a>
+            </li>
+            <li>
+              <Link href="/tos">TOS</Link>
+            </li>
+            <li>
+              <Link href="/privacy">Privacy Policy</Link>
+            </li>
           </ul>
         </nav>
 
-        <a href={botInvite} className="button primary">Add P2 Assistant</a>
-
-
+        <a href={botInvite} className={`button primary ${styles.add}`}>
+          Add P2 Assistant
+        </a>
+      </div>
     </div>
-    
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
